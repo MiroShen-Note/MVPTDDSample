64f224c074f8d82b909a0cb8723b9b1fffa942f7
LAPTOP-URKIRU4J\chtw1 <miro.job.research@gmail.com>
2023-11-23 13:58:25 +0800

    init commit

---
+++ app/src/main/java/miro/shen/research/mvptddsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package miro.shen.research.mvptddsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
