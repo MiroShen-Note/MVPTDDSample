86549cb0e1a813ec4801a0252672421c6c17bf72
LAPTOP-URKIRU4J\chtw1 <miro.job.research@gmail.com>
2023-11-28 13:59:01 +0800

    refactor loginIdVerify

--- app/src/main/java/miro/shen/research/mvptddsample/RegisterPresenter.kt
+++ app/src/main/java/miro/shen/research/mvptddsample/RegisterPresenter.kt
@@ -1,20 +1,22 @@
 package miro.shen.research.mvptddsample
 
-import java.util.Locale
-
 class RegisterPresenter(val view: RegisterContract.IRegisterView) : RegisterContract.IRegisterPresenter {
     override fun register(loginId: String, password: String) {
+
+        if (!loginVerify(loginId)) {
+            view.onInputDataError("錯誤", "帳號至少要6碼，第1碼為英文")
+        }
+    }
+
+    private fun loginVerify(loginId: String): Boolean {
         var isLoginIdOK = false
         //帳號至少6碼，第1碼為英文
-        if(loginId.length >= 6){
-            if(loginId.uppercase().first() in 'A'..'Z'){
+        if (loginId.length >= 6) {
+            if (loginId.uppercase().first() in 'A'..'Z') {
                 isLoginIdOK = true
             }
         }
-
-        if(!isLoginIdOK){
-            view.onInputDataError("錯誤","帳號至少要6碼，第1碼為英文")
-        }
+        return isLoginIdOK
     }
 
 }
